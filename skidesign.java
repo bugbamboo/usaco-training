d5c87c780ef537cdf827ccc306e4c566a340f47d
bugbamboo <39929404+bugbamboo@users.noreply.github.com>
2020-02-08 18:04:22 -0500

    Create skidesign.java

---
+++ skidesign.java
@@ -0,0 +1,36 @@
+
+import java.io.*;
+import java.util.*;
+
+public class skidesign {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader("skidesign.in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("skidesign.out")));
+        int n = Integer.parseInt(br.readLine());
+        int[] hills = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            hills[i]= Integer.parseInt(br.readLine());
+        }
+
+        int total = 1000000000;
+        for(int i = 0; i <= 83; i++) {
+            int cost = 0;
+            int d = 0;
+            for (int j = 0; j < n; j++) {
+                if (i>hills[j]) {
+                    d=i-hills[j];
+                } else if (i<hills[j]-17) {
+                    d=hills[j]-i-17;
+                } else {
+                    d = 0;
+                }
+                cost+=d*d;
+            }
+            total=cost;
+        }
+        out.println(total);
+        out.close();
+
+    }
+}
