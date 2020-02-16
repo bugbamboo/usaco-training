d7fe28512c8b5695fdbb281163b541139dc5545d
bugbamboo <39929404+bugbamboo@users.noreply.github.com>
2020-02-16 14:50:38 -0500

    Update ariprog.java

--- ariprog.java
+++ ariprog.java
@@ -3,71 +3,49 @@ import java.util.*;
 
 
 public class ariprog {
-
     public static void main(String[] args) throws IOException {
         BufferedReader f = new BufferedReader(new FileReader("ariprog.in"));
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ariprog.out")));
         int num = Integer.parseInt(f.readLine());
         int limit = Integer.parseInt(f.readLine());
-        Integer[] arr = generator(limit);
+        HashSet<Integer> arr = new HashSet<Integer>(generator(limit));
+        TreeSet<Integer> ar4 = new TreeSet<Integer>(arr);
+        Integer[] ar2= ar4.toArray(new Integer[arr.size()]);
+
         int e=0;
-        int z=(arr[arr.length-1]/(num-1));
-        int b;
-
-
+        int z=(ar2[ar2.length-1]/(num-1));
 
         for(int y=1;y<=z;y++){
-                b=Arrays.binarySearch(arr,arr[arr.length-1]-y*(num-1));
-                if(b<0){
-                    b=b*(-1)-1;
-                }
 
-
-            for (int i=0;i<=b;i++){
-
+            for (int i=0;i<ar2.length;i++){
                 int x=0;
-                A:for(int r=0; r<num;r++){
-
-                    if(!isValid(arr[i]+(y*r),arr)){
-                        break A;
+                for(int r=0; r<num;r++){
+                    if(!arr.contains(ar2[i]+(y*r))){
+                        break;
                     }else{
                         x++;
-
-
                     }
                 }
-                if(x==num){
-                    out.println(arr[i]+" "+y);
+                if(x>=num){
+                    out.println(ar2[i]+" "+y);
                     e++;
                 }
             }
         }
+        
         if(e==0){
             out.println("NONE");
         }
         out.close();
     }
 
-    static Integer[] generator(int limit){
-        HashSet<Integer> hset = new HashSet();
+    static HashSet<Integer> generator(int limit){
+        HashSet<Integer> hset= new HashSet<>();
         for (int i=0; i<=limit;i++){
-            for(int j=i; j<=limit;j++){
+            for(int j=0; j<=limit;j++){
                 hset.add(i*i+j*j);
             }
         }
-        Integer[] arr =hset.toArray(new Integer[hset.size()]);
-
-        Arrays.sort(arr);
-        System.out.println("1");
-        return arr;
-
+        return hset;
     }
-
-    public static boolean isValid(int i, Integer [] dictionary){
-        int y = Arrays.binarySearch(dictionary,i);
-        return y>=0;
-    }
-
-
-
 }
